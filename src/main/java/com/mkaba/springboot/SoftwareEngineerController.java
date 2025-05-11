fde3c5a4a814563fa15bd1c6d32f2b8a7f17606c
mehmetkaba <gm.mkaba@gmail.com>
2025-05-11 14:43:01 +0300

    initial commit

---
+++ src/main/java/com/mkaba/springboot/SoftwareEngineerController.java
@@ -0,0 +1,22 @@
+package com.mkaba.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getSoftwareEngineers() {
+        return List.of(
+                new SoftwareEngineer(1, "Mike", Collections.singletonList("js, react")),
+                new SoftwareEngineer(2, "James", Collections.singletonList("java, spring"))
+        );
+    }
+}
