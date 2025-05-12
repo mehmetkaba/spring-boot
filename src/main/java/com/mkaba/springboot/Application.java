48a3bc7bb55fd5b47358cdc4926549332dab5c65
mehmetkaba <gm.mkaba@gmail.com>
2025-05-12 21:14:23 +0300

    Controller Service Repository added

--- src/main/java/com/mkaba/springboot/Application.java
+++ src/main/java/com/mkaba/springboot/Application.java
@@ -6,16 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-    @GetMapping
-    public String helloWorld() {
-        return "Hello World Spring Boot 1";
-    }
-
 }
--- src/main/java/com/mkaba/springboot/SoftwareEngineerController.java
+++ src/main/java/com/mkaba/springboot/SoftwareEngineerController.java
@@ -1,23 +1,30 @@
 package com.mkaba.springboot;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/software-engineers")
 public class SoftwareEngineerController {
 
+
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
     @GetMapping
     public List<SoftwareEngineer> getSoftwareEngineers() {
-        return List.of(
-                new SoftwareEngineer(1, "Mike", Collections.singletonList("js, react")),
-                new SoftwareEngineer(2, "James", Collections.singletonList("java, spring"))
-        );
+//        return List.of(
+//                new SoftwareEngineer(1, "Mike", Collections.singletonList("js, react")),
+//                new SoftwareEngineer(2, "James", Collections.singletonList("java, spring"))
+//        );
+
+        return softwareEngineerService.getAllSoftwareEngineers();
     }
 
     //:)
